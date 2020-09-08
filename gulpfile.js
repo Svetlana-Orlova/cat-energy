3ad1a40a010524ec5e767ef93e763b72e050ecb1
Svetlana-Orlova <Svetlana-Orlova@github.com>
2020-09-08 17:40:31 +0300

    Правки по графике

--- gulpfile.js
+++ gulpfile.js
@@ -121,8 +121,8 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/*.html", gulp.series(build)).on("change", sync.reload);
 };
 
 exports.default = gulp.series(build, server, watcher);
