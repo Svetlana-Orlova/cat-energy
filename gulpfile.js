bb5e68a58eab15ec4d8b9d13a0ca8c4d95bf9772
Svetlana-Orlova <Svetlana-Orlova@github.com>
2020-09-12 01:20:32 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
